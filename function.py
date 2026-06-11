d30c7a51d0d97222c2592b5dd1163441cf45a3bb
Riyadulislamratul <riadul1810833319@gmail.com>
2026-06-11 21:08:10 +0600

    first commit

---
+++ function.py
@@ -0,0 +1,62 @@
+
+# Function Syntax
+# def function_name():
+#     statement
+# ----------- Parameter------
+# def greet():
+#     print("Hello World!")
+
+# greet()
+
+# ------------ Arguments----------
+# def greet(name):
+#     print("Hello" , name)
+
+# greet("Udoy")
+
+# Here name is a parameter and "Udoy" is an argument.
+
+
+# def sum(x, y):
+#     print(x+y)
+# sum(25, 50)
+
+# def multi(a, b):
+#     print(a*b)
+# multi(502, 60)
+
+# ------------- With Return Statement-----------
+
+# def square(x):
+#     print(x*x)   
+# result = square(7)
+# print(result)  
+
+# # Global Variable
+# a = 30
+
+# def square(x):
+#     b=5  # Local Variable
+#     return x*x
+# result = square(7)
+# print(result)  
+
+
+
+# --------*Args---------
+
+# def function_name(*args):
+#     statement
+
+# def show(*args):
+#     print(args)
+# show(1, 225, 30, 6, 5)
+
+
+
+# def total(*num):
+#     print(sum(num))
+# total(52,45,548,549,41,654,5496,6)
+# total(2,3)
+# total(65465,68468474)
+
---
+++ loop.py
@@ -0,0 +1,57 @@
+# DRY
+# Don't Repeat Youself
+
+
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+
+# -----------FOR----------
+# for variable in sequence:
+#     statement
+
+# for i in range(5):
+#     print("Hello World")
+
+# Range
+# Range has three properties
+# range(5, 100, 2) #starting value, ending value, steps
+
+
+# for i in range(11):
+#     print(i)
+
+
+# for i in range(1, 11, 3):
+#     print(i)
+
+
+# --------Nested Loop--------------
+
+# car = ["BMW", "Audi", "Mercedes", "Lexus"]
+# color = ["Black", "Red", "White"]
+
+# for i in car:
+#     for j in color:
+#         print(j, i)
+
+# ----------------Multiplication Table--------------
+# num = int(input("Enter a value for Multiplication table: "))
+# for i in range(1, 11):
+#     print(num, "X", i, "=", num*i)
+
+
+
+
+# while variable < 5:
+#     statement
+
+# ---------- While Loop------------
+
+# sum = 1
+
+# while sum < 130:
+#     print(sum)
+#     sum+=sum  
